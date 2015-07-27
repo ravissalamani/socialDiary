13fde80900d5c86a2fc49abd9141fa6734a4a28c
ravi_salaman <ravi_salamani@persistent.co.in>
2015-07-27 16:05:19 +0530

    init

---
+++ client/services/api.js
@@ -0,0 +1,16 @@
+angular.module('Instagram')
+    .factory('API', function($http) {
+
+      return {
+        getFeed: function() {
+          return $http.get('http://socialdiary.eu-gb.mybluemix.net/api/feed');
+        },
+        getMediaById: function(id) {
+          return $http.get('http://socialdiary.eu-gb.mybluemix.net/api/media/' + id);
+        },
+        likeMedia: function(id) {
+          return $http.post('http://socialdiary.eu-gb.mybluemix.net/api/like', { mediaId: id });
+        }
+      }
+
+    });
